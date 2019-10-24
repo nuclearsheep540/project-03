1a9e1c00578ddb2b4f9d10ffe8bb3413f05be06a
ldalrymple1 <dalrymple.lydia@gmail.com>
2019-10-24 19:43:03 +0100

    app.js looking good

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 console.log('front end up and running')
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+
+import Register from './components/auth/Register'
 
 const App = () => (
-  <div>
-    <h1>Skill Swap</h1>
-  </div>
+  <BrowserRouter>
+    <main>
+      <nav>
+        <Link to='/register'>Sign Up</Link>
+      </nav>
+      <Switch>
+        <Route path="/register" component={Register} />
+      </Switch>
+
+    </main>
+  </BrowserRouter>
+   
 )
 
 ReactDOM.render(
---
+++ src/components/auth/Register.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Register extends React.Component {
+  constructor() {
+    super()
+
+ 
+    
+  }
+
+
+
+  render() {
+    return (
+      <h1>Checking</h1>
+    )
+  }
+}
+
+export default Register
