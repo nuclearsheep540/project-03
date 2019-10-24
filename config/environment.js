f5031ee1584017db92c7f988b80567effe2fc58b
mattdavey <matt.davey540@me.com>
2019-10-24 16:52:06 +0100

    updated modules

---
+++ config/environment.js
@@ -0,0 +1,5 @@
+const port = 4000
+const dbURI = 'mongodb://localhost/skillswap'
+const secret = 'shh! not telling'
+
+module.exports = { dbURI, port, secret }
---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log('front end up and running')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div>
+    <h1>Skill Swap</h1>
+  </div>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
